eff7781ce2811d3b77c24f1f9195559102c229ab
HermelaM <hermela.bb@gmail.com>
2020-10-10 10:32:49 -0700

    update

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -1,43 +1,36 @@
-// UFO Sightings - Javascript
-
-// Variable Declaration
-let tbody = d3.select("tbody");
-// From data.js
+// from data.js
 var tableData = data;
 
-// Function Build Table
-function buildTable(data){
-    // Start By Clearing Existing Data
-    tbody.html("");
-    // Loop Through `data` 
-    data.forEach((dataRow) => {
-        // Append Table Row `tr` to the Table Body `tbody`
-        let row = tbody.append("tr");
-        // `Object.values` & `forEach` to Iterate Through Values
-        Object.values(dataRow).forEach((val) => {
-            // Append a Cell to the Row for Each Value
-            let cell = row.append("td");
-            cell.text(val);
-        });
-    })
-}
-// Event that Triggers a Function When the Button is Clicked
-function handleClick(){
-    // Prevents the Page from Refreshing
-    d3.event.preventDefault();
-    // Select HTML Input Element & Get the Value Property of that Input Element
-    let date = d3.select("#datetime").property("value");
-    let filterData = tableData;
+// get table references
+var tbody = d3.select("tbody");
 
-    // Check if a Date was Entered & Filter Data Using that Date;
-    if(date) {
-        // Apply Filter to the Table Data to Only Keep Rows Where datetime Value Matches the Filter Value
-        filterData = filterData.filter((row) => row.datetime === date);
-    }
-    // Build Table with Filtered Data
-    buildTable(filterData);
+function buildTable(data) {
+  
+  tbody.html("");
+  data.forEach((dataRow) => {
+    var row = tbody.append("tr");
+    Object.values(dataRow).forEach((val) => {
+      var cell = row.append("td");
+        cell.text(val);
+      }
+    );
+  });
 }
-// `on` Function to attach an Event to the Handler Function
+
+function handleClick() {
+
+  // Grab the datetime value from the filter
+  var date = d3.select("#datetime").property("value");
+  let filteredData = tableData;
+
+  if (date) {
+    filteredData = filteredData.filter(row => row.datetime === date);
+  }
+
+  buildTable(filteredData);
+}
+
 d3.selectAll("#filter-btn").on("click", handleClick);
-// Build Table with data.js 
+
+// Build the table when the page loads
 buildTable(tableData);
